6215672eda081c87d819b8ed6ece417c5862bfee
Michael Clark <michaeljclark@mac.com>
2020-01-16 12:16:39 +1300

    Add threads and condition variables

--- avar.c
+++ avar.c
@@ -14,7 +14,19 @@
 inline long compare_and_swap(long volatile *t, long o, long n) {
     return InterlockedCompareExchange(t,n,o);
 }
+typedef unsigned thread_t;
+typedef CONDITION_VARIABLE cond_t;
 typedef CRITICAL_SECTION mutex_t;
+inline int thread_create(thread_t *thread, unsigned (__stdcall *threadfunc) (void*), void* arg) {
+    return (int)_beginthreadex(NULL, 0, threadfunc, arg, 0, thread);
+}
+#define thread_exit(rc) _endthreadex(rc)
+inline void thread_join(thread_t *thread) { WaitForSingleObject( thread, INFINITE ); CloseHandle(&thread); }
+inline void cond_init(cond_t *cond) { InitializeConditionVariable(cond); }
+inline void cond_destroy(cond_t *cond) { }
+inline void cond_wait(cond_t *cond, mutex_t *mutex) { SleepConditionVariableCS(cond, mutex, INFINITE); }
+inline void cond_signal(cond_t *cond) { WakeConditionVariable(cond); }
+inline void cond_broadcast(cond_t *cond) { WakeAllConditionVariable(cond); }
 inline void memory_barrier() { MemoryBarrier(); }
 inline void mutex_init(mutex_t *mutex) { InitializeCriticalSection(mutex); }
 inline void mutex_destroy(mutex_t *mutex) { DeleteCriticalSection(mutex); }
@@ -25,7 +37,21 @@ inline void memory_barrier() { asm volatile("": : :"memory"); }
 inline long compare_and_swap(long volatile *t, long o, long n) {
     return __sync_val_compare_and_swap(t,o,n);
 }
+typedef pthread_t thread_t;
+typedef pthread_cond_t cond_t;
 typedef pthread_mutex_t mutex_t;
+inline int thread_create(thread_t *thread, void* (*threadfunc)(void*), void* arg) {
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    return (int)pthread_create(thread, &attr, threadfunc, arg);
+}
+#define thread_exit(rc) do { pthread_exit((void*)rc); return NULL } while (0);
+inline void thread_join(thread_t *thread) { void *result; pthread_join(*thread, &result); }
+inline void cond_init(cond_t *cond) { pthread_cond_init(cond, NULL); }
+inline void cond_destroy(cond_t *cond) { pthread_cond_destroy(cond); }
+inline void cond_wait(cond_t *cond, mutex_t *mutex) { pthread_cond_wait(cond, mutex); }
+inline void cond_signal(cond_t *cond) { pthread_cond_signal(cond); }
+inline void cond_broadcast(cond_t *cond) { pthread_cond_broadcast(cond); }
 inline void mutex_init(mutex_t *mutex) { pthread_mutex_init(mutex, NULL); }
 inline void mutex_destroy(mutex_t *mutex) { pthread_mutex_destroy(mutex); }
 inline void mutex_lock(mutex_t *mutex) { pthread_mutex_lock(mutex); }
